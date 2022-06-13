25c48d29fe10fd8217941503899d3d5f78ddab4e
Reo Ono <1992reo@gmail.com>
2022-06-13 09:49:19 +0900

    fix

--- jupyter_server/auth/login.py
+++ jupyter_server/auth/login.py
@@ -107,7 +107,7 @@ class LoginHandler(JupyterHandler):
         cookie_options.setdefault("path", handler.base_url)
         handler.set_secure_cookie(handler.cookie_name, user_id, **cookie_options)
         # To set XSRF token at login
-        self.xsrf_token
+        handler.xsrf_token
         return user_id
 
     auth_header_pat = re.compile(r"token\s+(.+)", re.IGNORECASE)
